9c39d3bc3dce25728a7bfb0a9b95c650ab4e9c88
David Robinson <david.pl.robinson@googlemail.com>
2017-07-14 21:06:51 +1000

    initial

---
+++ general/docker_install.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+# Tested and working on ubuntu. 
+
+apt-get update
+apt-get install -y --no-install-recommends linux-image-extra-$(uname -r) linux-image-extra-virtual
+apt-get install -y --no-install-recommends apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
+add-apt-repository "deb https://apt.dockerproject.org/repo/ ubuntu-$(lsb_release -cs) main"
+apt-get update
+apt-get -y install docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+chmod +x /usr/bin/docker-compose
+update-rc.d docker defaults
+update-rc.d docker enable
+service docker start
+docker run hello-world
---
+++ git_set_remote.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --global user.name "David Robinson"
+git config --global user.email "david.pl.robinson@googlemail.com"
+git config credential.helper store
+git remote add docker https://github.com/davidplrobinson/scripts.git
