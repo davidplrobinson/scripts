bc5b50c263258c053a4ed78991b3f0de094fd154
David Robinson <david.pl.robinson@googlemail.com>
2017-07-14 21:19:51 +1000

    updates

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # General scripts I find useful.
+
+#### git_set_remote.sh - sets the remote repository, customomised for this folder, but edit to suit. 
+
+#### general/install_docker.sh - installs docker onto ubuntu systems, sets docker to auto-start and pulls hello-world to test. 
--- git_set_remote.sh
+++ git_set_remote.sh
@@ -3,4 +3,4 @@
 git config --global user.name "David Robinson"
 git config --global user.email "david.pl.robinson@googlemail.com"
 git config credential.helper store
-git remote add docker https://github.com/davidplrobinson/scripts.git
+git remote add scripts https://github.com/davidplrobinson/scripts.git
