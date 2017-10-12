9874d83caa2d1b53774574a5e4d1f128d8972f3f
David Robinson <david.pl.robinson@googlemail.com>
2017-10-13 09:25:05 +1100

    added delay

--- general/kubernetes/step2.sh
+++ general/kubernetes/step2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo snap install lxd
+sleep 5
 /snap/bin/lxd init --auto
 /snap/bin/lxc network create lxdbr0 ipv4.address=auto ipv4.nat=true ipv6.address=none ipv6.nat=false
 conjure-up kubernetes
