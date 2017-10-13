1fac164170d734dc22e11e11826beb14975ee7ec
David Robinson <david.pl.robinson@googlemail.com>
2017-10-13 16:46:31 +1100

    added delay, stopped cluster from deleting itself after helloworld

--- general/kubernetes/minikube-helloworld.sh
+++ general/kubernetes/minikube-helloworld.sh
@@ -4,5 +4,6 @@ minikube start --vm-driver kvm
 minikube status
 kubectl run hello-minikube --image=gcr.io/google_containers/echoserver:1.4 --port=8080
 kubectl expose deployment hello-minikube --type=NodePort
+sleep 5
 curl $(minikube service hello-minikube --url)
-minikube stop
+#minikube stop
