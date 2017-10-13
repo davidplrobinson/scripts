fb480ae5bda48ae42cac206ffa8b7998fffb8bbd
David Robinson <david.pl.robinson@googlemail.com>
2017-10-13 16:33:25 +1100

    updated to reference minikube

---
+++ general/kubernetes/minikube-helloworld.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+minikube start --vm-driver kvm
+minikube status
+kubectl run hello-minikube --image=gcr.io/google_containers/echoserver:1.4 --port=8080
+kubectl expose deployment hello-minikube --type=NodePort
+curl $(minikube service hello-minikube --url)
+minikube stop
