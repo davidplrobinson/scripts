2273f5e744fb3afa2e56f24a820a4a336b987ce1
David Robinson <david.pl.robinson@googlemail.com>
2017-10-13 11:58:10 +1000

    Update install_kubernetes.sh

--- general/kubernetes/install_kubernetes.sh
+++ general/kubernetes/install_kubernetes.sh
@@ -4,10 +4,16 @@ sudo apt-get install -y libvirt-bin qemu-kvm
 sudo usermod -a -G libvirtd $(whoami)
 newgrp libvirtd
 
+curl -L https://github.com/docker/machine/releases/download/v0.12.2/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine
+chmod +x /tmp/docker-machine
+sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
+
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
 kubectl cluster-info
 
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.22.3/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.22.3/minikube-linux-amd64
+chmod +x minikube
+sudo mv minikube /usr/local/bin/
