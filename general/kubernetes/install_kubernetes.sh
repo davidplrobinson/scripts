bb7a094513be2f41c4f750bbe612eece97ba0fa8
David Robinson <david.pl.robinson@googlemail.com>
2017-10-13 11:41:45 +1000

    Update install_kubernetes.sh

--- general/kubernetes/install_kubernetes.sh
+++ general/kubernetes/install_kubernetes.sh
@@ -7,3 +7,5 @@ chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
 kubectl cluster-info
+
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.22.3/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
