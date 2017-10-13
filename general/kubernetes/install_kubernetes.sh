8dcc9a29c50de42f776390d4637a9b634c5c9925
David Robinson <david.pl.robinson@googlemail.com>
2017-10-13 14:42:01 +1100

    tidy up script

--- general/kubernetes/install_kubernetes.sh
+++ general/kubernetes/install_kubernetes.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+# install kvm
 sudo apt-get install -y libvirt-bin qemu-kvm
 sudo usermod -a -G libvirtd $(whoami)
 newgrp libvirtd
 
+# install docker
 curl -L https://github.com/docker/machine/releases/download/v0.12.2/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine
 chmod +x /tmp/docker-machine
 sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
 
+# install kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
 kubectl cluster-info
 
+# install minikube
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.22.3/minikube-linux-amd64
 chmod +x minikube
 sudo mv minikube /usr/local/bin/
