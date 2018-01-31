a7da608ac5d417b6da87edd6341911984f3864db
David Robinson <david.pl.robinson@googlemail.com>
2018-01-31 14:30:17 +1100

    Update setup_mono.sh

--- general/mono/setup_mono.sh
+++ general/mono/setup_mono.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb http://download.mono-project.com/repo/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/mono-official.list
 apt-get update
