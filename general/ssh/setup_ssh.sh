1e85e548192a5517b6b84fd489733b13560217bb
David Robinson <david.pl.robinson@googlemail.com>
2018-01-31 12:47:15 +1100

    added ssh install, cleaned up a little

---
+++ general/ssh/setup_ssh.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$EUID" -eq 0 ]
+	then echo "Please DO NOT run as root"
+	exit
+fi
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y ssh
+
+mkdir ~/.ssh
+cat davidplrobinson.pub >~/.ssh/authorized_keys
+chmod 700 ~/.ssh -R
