0dc4de0362cd226ef017052f9d95636578d37ca1
David Robinson <david.pl.robinson@googlemail.com>
2018-01-31 13:21:41 +1100

    Update git_set_remote.sh

--- git_set_remote.sh
+++ git_set_remote.sh
@@ -3,4 +3,5 @@
 git config --global user.name "David Robinson"
 git config --global user.email "david.pl.robinson@googlemail.com"
 git config --global credential.helper store
+git config --global push.default matching
 git remote add scripts https://github.com/davidplrobinson/scripts.git
