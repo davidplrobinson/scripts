7ea92b9bd8c93a484d65961005b9fe35dfaea690
David Robinson <david.pl.robinson@googlemail.com>
2018-01-31 14:29:55 +1100

    Update setup_minikube.sh

--- general/kubernetes/setup_minikube.sh
+++ general/kubernetes/setup_minikube.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 # install kvm
 sudo apt-get install -y libvirt-bin qemu-kvm
 sudo usermod -a -G libvirtd $(whoami)
