2d7dc82f022acbf72432634074d8aa8995ce07a2
David Robinson <david.pl.robinson@googlemail.com>
2018-02-03 17:07:35 +1100

    added parameters to buildvm.sh

--- general/kvm/buildvm.sh
+++ general/kvm/buildvm.sh
@@ -1,1 +1,8 @@
-sudo virt-install -n test2 --ram=512 --vcpu=1 --disk pool=default,format=qcow2,size=5 --graphics vnc,password=password --cdrom /mnt/data/isos/ubuntu-17.10.1-server-amd64.iso --network bridge:virbr100
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+	then echo "Please run as root"
+	exit
+fi
+
+virt-install -n test --ram=512 --vcpu=1 --disk pool=default,format=qcow2,size=5 --graphics vnc,password=password --cdrom /mnt/data/isos/ubuntu-17.10.1-server-amd64.iso --network bridge:virbr100 --console pty,target_type=virtio
