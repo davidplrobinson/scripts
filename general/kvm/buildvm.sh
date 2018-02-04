5eb4e30d7ff29b386c58e8c44cac49226f910cfb
David Robinson <david.pl.robinson@googlemail.com>
2018-02-04 12:09:33 +1100

    added random string to vm names

--- general/kvm/buildvm.sh
+++ general/kvm/buildvm.sh
@@ -5,4 +5,9 @@ if [ "$EUID" -ne 0 ]
 	exit
 fi
 
-virt-install -n test --ram=512 --vcpu=1 --disk pool=default,format=qcow2,size=5 --graphics vnc,password=password --cdrom /mnt/data/isos/ubuntu-17.10.1-server-amd64.iso --network bridge:virbr100 --console pty,target_type=virtio
+randomid=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 6 | head -n 1)
+
+virt-install -n test-$randomid --ram=512 --vcpu=1 --disk pool=default,format=qcow2,size=5 \
+	--graphics vnc,listen=0.0.0.0,password=password --noautoconsole \
+	--cdrom /mnt/data/isos/ubuntu-17.10.1-server-amd64.iso --network bridge:virbr100 \
+	--console "pty,target_type=serial"
