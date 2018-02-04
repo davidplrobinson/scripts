976a4aac23aac3c20cbbb905ef4e3eb213afed75
David Robinson <david.pl.robinson@googlemail.com>
2018-02-04 13:43:34 +1100

    updated build script

--- general/kvm/buildvm.sh
+++ general/kvm/buildvm.sh
@@ -9,7 +9,7 @@ randomid=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)
 
 virt-install -n test-$randomid --ram=512 --vcpu=1 --disk pool=datastore_1,format=qcow2,size=5 \
 	--graphics vnc,listen=0.0.0.0,password=password --noautoconsole \
-	--cdrom /mnt/data/isos/ubuntu-17.10.1-server-amd64.iso --network bridge:virbr100 \
+	--cdrom /mnt/data/isos/ubuntu-17.10.1-server-amd64.iso --network bridge=virbr100 \
 	--console "pty,target_type=serial"
 
 virsh autostart test-$randomid
