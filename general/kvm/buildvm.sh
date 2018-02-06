21475471d4c701927bb9d48080138e6a51dd5183
David Robinson <david.pl.robinson@googlemail.com>
2018-02-06 13:39:29 +1100

    updated network settings and test build file

--- general/kvm/buildvm.sh
+++ general/kvm/buildvm.sh
@@ -8,9 +8,9 @@ fi
 randomid=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 4 | head -n 1)
 servername=test-$randomid
 
-virt-install -n $servername --ram=512 --vcpu=1 --disk pool=datastore_1,format=qcow2,size=5 \
+virt-install -n $servername --ram=2048 --vcpu=1 --disk pool=datastore_1,format=qcow2,size=10 \
 	--graphics vnc,listen=0.0.0.0,password=password --noautoconsole \
-	--cdrom something.iso --network bridge=virbr100 \
+	--cdrom /mnt/data/isos/something.iso --network bridge=br37 \
 	--console "pty,target_type=serial" --wait=-1
 
 virsh autostart $servername
--- general/kvm/setup_kvm_network.sh
+++ general/kvm/setup_kvm_network.sh
@@ -6,7 +6,11 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 virsh net-define ./kvm_internal_network.xml
+virsh net-define ./kvm_external_network.xml
 virsh net-autostart internalnet
+virsh net-autostart externalnet
 virsh net-start internalnet
+virsh net-start externalnet
 virsh net-list --all
 virsh net-info internalnet
+virsh net-info externalnet
