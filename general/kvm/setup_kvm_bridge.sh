5d0b6fa25d2f2e2cb90b7ee40ac2d84379ac0df2
David Robinson <david.pl.robinson@googlemail.com>
2018-02-14 16:43:12 +1100

    comments

--- general/kvm/setup_kvm_bridge.sh
+++ general/kvm/setup_kvm_bridge.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# note - this script isn't finished, it will disconnect the network, to resolve use -
+#
+# sudo brctl delif br0 ens33
+# sudo brctl delbr br0
+
 if [ "$EUID" -ne 0 ]; then
 	echo "Please run as root"
 	exit
